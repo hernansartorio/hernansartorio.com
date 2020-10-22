7337239104e3fb9bd8e8176538123b47b583e107
Hernán Sartorio <hernan124@gmail.com>
2020-10-21 23:08:51 -0300

    Add Fathom

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as Fathom from 'fathom-client'
 import '../styles/style.scss'
 
-function MyApp({ Component, pageProps }) {
+function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    Fathom.load('YIFVTXOS', {
+      includedDomains: ['hernansartorio.com']
+    })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
-
-export default MyApp
+export default App
