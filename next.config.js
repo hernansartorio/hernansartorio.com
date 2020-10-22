53821b015340ef29a406d55f70104989ced21f74
Hernán Sartorio <hernan124@gmail.com>
2020-10-21 23:31:24 -0300

    Add redirect

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/jquery-nice-select',
+        destination: 'https://jqueryniceselect.hernansartorio.com/',
+        permanent: true,
+      },
+    ]
+  },
+}
