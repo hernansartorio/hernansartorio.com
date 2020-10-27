47ae3abdc7948c62a5de242f096985d1e6ef0593
Hernán Sartorio <hernan124@gmail.com>
2020-10-27 20:42:10 -0300

    Revert "Add Fathom"
    This reverts commit 7337239104e3fb9bd8e8176538123b47b583e107.

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,7 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import * as Fathom from 'fathom-client'
 import '../styles/style.scss'
 
 function App({ Component, pageProps }) {
-  const router = useRouter()
-
-  useEffect(() => {
-    Fathom.load('YIFVTXOS', {
-      includedDomains: ['hernansartorio.com']
-    })
-
-    function onRouteChangeComplete() {
-      Fathom.trackPageview()
-    }
-
-    router.events.on('routeChangeComplete', onRouteChangeComplete)
-
-    return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete)
-    }
-  }, [])
-
   return <Component {...pageProps} />
 }
+
 export default App
